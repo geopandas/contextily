858effe99ced02c8936052a101a122d5640eedbf
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-09-16 21:07:41 +0100

    Switching tests to tolerance 7th decimal

--- tests/test_ctx.py
+++ tests/test_ctx.py
@@ -10,10 +10,12 @@ def test_bounds2raster():
     _ = ctx.bounds2raster(w, s, e, n, 4, 'test.tif', ll=True)
     rtr = rio.open('test.tif')
     img = ctx.np.array([ band for band in rtr.read() ]).transpose(1, 2, 0)
-    assert tuple(rtr.bounds) == (-12528334.684053527,
-                                  2509580.5126589066,
-                                 -10023646.141204873,
-                                  5014269.05550756)
+    solu = (-12528334.684053527,
+             2509580.5126589066,
+            -10023646.141204873,
+             5014269.05550756)
+    for i,j in zip(rtr.bounds, solu):
+        assert round(i-j, 7) == 0
     assert img[100, 100, :].tolist() == [230, 229, 188]
     assert img[100, 200, :].tolist() == [156, 180, 131]
     assert img[200, 100, :].tolist() == [230, 225, 189]
@@ -22,10 +24,12 @@ def test_bounds2img():
     w, s, e, n = (-106.6495132446289, 25.845197677612305, 
             -93.50721740722656, 36.49387741088867)
     img, ext = ctx.bounds2img(w, s, e, n, 4, ll=True)
-    assert ext == (-12523442.714243276,
-                   -10018754.171394622,
-                    2504688.5428486555,
-                    5009377.085697309)
+    solu = (-12523442.714243276,
+             -10018754.171394622,
+              2504688.5428486555,
+              5009377.085697309)
+    for i,j in zip(ext, solu):
+        assert round(i-j, 7) == 0
     assert img[100, 100, :].tolist() == [230, 229, 188]
     assert img[100, 200, :].tolist() == [156, 180, 131]
     assert img[200, 100, :].tolist() == [230, 225, 189]
