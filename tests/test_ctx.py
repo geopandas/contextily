b1976c3613c5ef85913aa1b3e2ecb009a5a5d2e5
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-09-16 21:08:20 +0100

    Switching tests to tolerance 7th decimal

--- tests/test_ctx.py
+++ tests/test_ctx.py
@@ -4,6 +4,8 @@ import contextily as ctx
 import mercantile as mt
 import rasterio as rio
 
+TOL = 7
+
 def test_bounds2raster():
     w, s, e, n = (-106.6495132446289, 25.845197677612305, 
             -93.50721740722656, 36.49387741088867)
@@ -15,7 +17,7 @@ def test_bounds2raster():
             -10023646.141204873,
              5014269.05550756)
     for i,j in zip(rtr.bounds, solu):
-        assert round(i-j, 7) == 0
+        assert round(i-j, TOL) == 0
     assert img[100, 100, :].tolist() == [230, 229, 188]
     assert img[100, 200, :].tolist() == [156, 180, 131]
     assert img[200, 100, :].tolist() == [230, 225, 189]
@@ -29,7 +31,7 @@ def test_bounds2img():
               2504688.5428486555,
               5009377.085697309)
     for i,j in zip(ext, solu):
-        assert round(i-j, 7) == 0
+        assert round(i-j, TOL) == 0
     assert img[100, 100, :].tolist() == [230, 229, 188]
     assert img[100, 200, :].tolist() == [156, 180, 131]
     assert img[200, 100, :].tolist() == [230, 225, 189]
@@ -59,7 +61,7 @@ def test__sm2ll():
     maxX, maxY = ctx._sm2ll(e, n)
     nw, ns = mt.xy(minX, minY)
     ne, nn = mt.xy(maxX, maxY)
-    assert round(nw - w, 7) == 0
-    assert round(ns - s, 7) == 0
-    assert round(ne - e, 7) == 0
-    assert round(nn - n, 7) == 0
+    assert round(nw - w, TOL) == 0
+    assert round(ns - s, TOL) == 0
+    assert round(ne - e, TOL) == 0
+    assert round(nn - n, TOL) == 0
