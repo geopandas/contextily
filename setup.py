eb99e3b74dc9bc003f2de5f128a639e4b614f0c0
ocefpaf <ocefpaf@gmail.com>
2016-09-28 19:27:35 -0300

    Read dependencies from file

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
+# Dependencies.
+with open('requirements.txt') as f:
+    tests_require = f.readlines()
+install_requires = [t.strip() for t in tests_require]
+
 setup(name='contextily',
-        version='0.9.0',
+      version='0.9.0',
       description='Context geo-tiles in Python',
       url='https://github.com/darribas/contextily',
       author='Dani Arribas-Bel',
       author_email='daniel.arribas.bel@gmail.com',
       license='3-Clause BSD',
       packages=['contextily'],
-      install_requires=['cartopy', 'pandas', 'PIL', 
-          'rasterio', 'six', 'mercantile'],
+      install_requires=install_requires,
       package_data={'cenpy': ['stfipstable.csv']},
       zip_safe=False)
