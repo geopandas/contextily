f7312faca07a2b14ea8204287b3a81b782aa2ec8
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2018-07-14 22:02:48 +0100

    Adding basic implementation of add_basemap with web tiles

--- contextily/plotting.py
+++ contextily/plotting.py
@@ -1,14 +1,34 @@
 """Tools to plot basemaps"""
 
-def add_basemap(ax, zoom='auto', url=sources.ST_Terrain, 
-		interpolation='sinc', attribution_text = ‘’, 
+from . import tile_providers as sources
+from .tile import _calculate_zoom, bounds2img, _sm2ll
+
+def add_basemap(ax, zoom='auto', url=sources.ST_TERRAIN, 
+		interpolation='bilinear', attribution_text = '', 
                 **extra_imshow_args):
-    if zoom.lower() == 'auto':
-        zoom = calculate_zoom()
-    if url is filepath:
+    '''
+    Add a basemap to `ax`
+
+    NOTE: `ax` needs to be in Web Mercator
+    '''
+    # Extent
+    left, right = ax.get_xlim()
+    bottom, top = ax.get_ylim()
+    # If web source
+    if url[:4] == 'http':
+        # Zoom
+        if (type(zoom) == str) and (zoom.lower() == 'auto'):
+            min_ll = _sm2ll(left, bottom)
+            max_ll = _sm2ll(right, top)
+            zoom = _calculate_zoom(*min_ll, *max_ll)
+        image, extent = bounds2img(left, bottom, right, top,
+                                   zoom=zoom, url=url, ll=False)
+    # If local source
+    else:
         import rasterio
         image = rasterio.read_and_cut_raster_to_axis_bbox(url)
-    else:
-        image = fetch_tile_from_provider(zoom,url,...)
-    ax.imshow(image, interpolation=interpolation, **extra_imshow_args)
+    # Plotting
+    ax.imshow(image, extent=extent, 
+              interpolation=interpolation, **extra_imshow_args)
     return ax
+
