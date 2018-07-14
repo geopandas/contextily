437ad98024846aa1c31c41067db5d5a68db4175c
ljwolf <levi.john.wolf@gmail.com>
2018-07-14 12:04:04 -0500

    update test format

--- tests/test_ctx.py
+++ tests/test_ctx.py
@@ -13,7 +13,7 @@ TOL = 7
 def test_bounds2raster():
     w, s, e, n = (-106.6495132446289, 25.845197677612305,
             -93.50721740722656, 36.49387741088867)
-    _ = ctx.bounds2raster(w, s, e, n, 4, 'test.tif', ll=True)
+    _ = ctx.bounds2raster(w, s, e, n, 'test.tif', zoom=4, ll=True)
     rtr = rio.open('test.tif')
     img = np.array([ band for band in rtr.read() ]).transpose(1, 2, 0)
     solu = (-12528334.684053527,
@@ -29,7 +29,7 @@ def test_bounds2raster():
     # multiple tiles for which result is not square
     w, s, e, n = (2.5135730322461427, 49.529483547557504,
                   6.15665815595878, 51.47502370869813)
-    raster, _ = ctx.bounds2raster(w, s, e, n, 7, 'test.tif', ll=True)
+    raster, _ = ctx.bounds2raster(w, s, e, n, 'test.tif', zoom=7, ll=True)
     rtr = rio.open('test.tif')
     img = np.array([ band for band in rtr.read() ]).transpose(1, 2, 0)
     assert raster.shape == img.shape
@@ -37,7 +37,7 @@ def test_bounds2raster():
 def test_bounds2img():
     w, s, e, n = (-106.6495132446289, 25.845197677612305,
             -93.50721740722656, 36.49387741088867)
-    img, ext = ctx.bounds2img(w, s, e, n, 4, ll=True)
+    img, ext = ctx.bounds2img(w, s, e, n, zoom=4, ll=True)
     solu = (-12523442.714243276,
              -10018754.171394622,
               2504688.5428486555,
@@ -53,7 +53,7 @@ def test_howmany():
             -93.50721740722656, 36.49387741088867)
     zoom = 7
     expected = 25
-    got = ctx.howmany(w, s, e, n, zoom, verbose=False, ll=True)
+    got = ctx.howmany(w, s, e, n, zoom=zoom, verbose=False, ll=True)
     assert got == expected
 
 def test_ll2wdw():
@@ -61,7 +61,7 @@ def test_ll2wdw():
             -93.50721740722656, 36.49387741088867)
     hou = (-10676650.69219051, 3441477.046670125,
            -10576977.7804825, 3523606.146650609)
-    _ = ctx.bounds2raster(w, s, e, n, 4, 'test.tif', ll=True)
+    _ = ctx.bounds2raster(w, s, e, n, 'test.tif', zoom=4, ll=True)
     rtr = rio.open('test.tif')
     wdw = ctx.tile.bb2wdw(hou, rtr)
     assert wdw == ((152, 161), (189, 199))
