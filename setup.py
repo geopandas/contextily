9a8b652768365b24fbce912b71737f4fa6b3295c
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2020-05-14 08:35:34 +0000

    Fix #149

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=install_requires,
     zip_safe=False,
 )
