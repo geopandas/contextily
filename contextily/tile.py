Fig bug in auto computation of zoom levels (#214)
For maps with large aspect ratio, contextily downloads tiles as if the map was
as small as the smallest dimension. It should base the zoom level on the
dimension that's a larger extent.

contextily/tile.py
@@ -531,7 +531,7 @@ def _calculate_zoom(w, s, e, n):
     # Calculate the zoom
     zoom_lon = np.ceil(np.log2(360 * 2.0 / lon_length))
     zoom_lat = np.ceil(np.log2(360 * 2.0 / lat_length))
-    zoom = np.max([zoom_lon, zoom_lat])
+    zoom = np.min([zoom_lon, zoom_lat])
     return int(zoom)
 
 