74deb0e0bbd01fd9ca6e2c926725c498d4114eae
Martin Fleischmann <martin@martinfleischmann.net>
2024-04-11 10:10:23 +0200

    TST: remove callable test

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,8 +1,6 @@
-import xyzservices
 import contextily as cx
 
 import pytest
-from numpy.testing import assert_allclose
 
 
 @pytest.mark.network
@@ -19,18 +17,6 @@ def test_providers():
         cx.providers.NASAGIBS.ViirsEarthAtNight2012,
     ]:
         cx.bounds2img(w, s, e, n, 4, source=provider, ll=True)
-
-
-def test_providers_callable():
-    # only testing the callable functionality to override a keyword, as we
-    # cannot test the actual providers that need an API key
-    updated_provider = cx.providers.GeoportailFrance.plan(apikey="mykey")
-    assert isinstance(updated_provider, xyzservices.TileProvider)
-    assert "url" in updated_provider
-    assert updated_provider["apikey"] == "mykey"
-    # check that original provider dict is not modified
-    assert cx.providers.GeoportailFrance.plan["apikey"] == "essentiels"
-
 
 def test_invalid_provider():
     w, s, e, n = (-106.649, 25.845, -93.507, 36.494)
