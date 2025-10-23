cf06b92d5d39852ba3b8aa9fced68f0cd1bc844c
philnagel <phil.nagel@bolton-menk.com>
2025-10-23 09:40:49 -0500

    feat: use ThreadPoolExecutor instead of joblib
    there are known issues with using tqdm along with some joblib backends. using ThreadPoolExecutor instead.

--- contextily/progress.py
+++ contextily/progress.py
@@ -11,23 +11,46 @@ def set_progress_bar(progress_bar=None):
     Parameters
     ----------
     progress_bar : class, optional
-        A tqdm-compatible progress bar class. If None, uses the default tqdm.
+        A tqdm-compatible progress bar class. If None, progress bar is disabled.
         The progress bar class should have the same interface as tqdm.
         Common alternatives include:
         - tqdm.notebook.tqdm for Jupyter notebooks
         - custom implementations with the same interface
     """
     global _progress_bar
-    _progress_bar = progress_bar if progress_bar is not None else _default_progress_bar
+    _progress_bar = progress_bar
 
 
 def get_progress_bar():
     """
     Returns the progress bar class to be used for downloading tiles.
+    If progress bars are disabled (set to None), returns a no-op context
+    manager that doesn't display progress.
 
     Returns
     ----------
-    progress_bar : class
-        A tqdm-compatible progress bar class.
+    progress_bar : callable
+        A callable that returns either a tqdm-compatible progress bar or a
+        no-op context manager with update/close methods if progress bars
+        are disabled.
     """
+    if _progress_bar is None:
+        class NoOpProgress:
+            def __init__(self, *args, **kwargs):
+                self.context = nullcontext()
+                
+            def __enter__(self):
+                self.context.__enter__()
+                return self
+                
+            def __exit__(self, *args):
+                self.context.__exit__(*args)
+                
+            def update(self, n=1):
+                pass
+                
+            def close(self):
+                pass
+                
+        return lambda *args, **kwargs: NoOpProgress(*args, **kwargs)
     return _progress_bar
