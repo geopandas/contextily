4cfa350f104c8d6c319229387b5ea973b8eee75d
jthetzel <jthetzel@gmail.com>
2025-11-23 08:25:31 -0600

    test: Update expected sums

--- tests/test_cx.py
+++ tests/test_cx.py
@@ -617,8 +617,8 @@ def test_add_basemap_auto_zoom():
                 4852834.0517692715,
                 4891969.810251278,
             ),
-            764077703,
-            1031464583,
+            764120533,
+            1031507413,
             (1024, 1024, 4),
         ),
         # zoom_adjust and expected values where zoom_adjust == -1
